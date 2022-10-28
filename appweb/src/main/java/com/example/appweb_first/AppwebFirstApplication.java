9c77afa08285677d34e05209eb4eaecec185bd32
Your Name <you@example.com>
2022-10-28 13:45:29 +0300

    first commit

---
+++ appweb/src/main/java/com/example/appweb_first/AppwebFirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.appweb_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppwebFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppwebFirstApplication.class, args);
+	}
+
+}
---
+++ appweb/src/test/java/com/example/appweb_first/AppwebFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appweb_first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppwebFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
